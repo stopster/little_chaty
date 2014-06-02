3c96f3125c08bea122fd1a30d9fb141406baf0e4
stopster <stopster17@gmail.com>
2014-06-02 08:52:06 +0300

    Bower, requirejs, Backbone used. Make some basics.

--- client.js
+++ client.js
@@ -3,16 +3,7 @@ exports.Client = function(app){
 	app.set("view engine", "jade");
 
 	app.get("/", function(req, res){
-		res.send("Chat app");
+		res.render("client");
 	});
-
-	var clientPath = process.env.FE_ACADEMY_CHAT_CLIENT_PATH;
-	var secretPublic = process.env.FE_ACADEMY_CHAT_CLIENT_DIR;
-
-	if(clientPath){
-		app.get("/" + clientPath, function(req, res){
-			res.render("client", {secretPublic: secretPublic});
-		});
-	}
 
 };
